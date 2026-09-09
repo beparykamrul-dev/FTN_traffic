c288e305c01e55aa0c4f2511a54458efbd34198d
Kamrul islam <227916250+beparykamrul-dev@users.noreply.github.com>
2026-09-09 16:23:04 -0600

    enforce runtime argument validation

--- internal/dataplane/runtime_command.go
+++ internal/dataplane/runtime_command.go
@@ -1,13 +1,8 @@
 package dataplane
 
 import (
-	"context"
-	"os/exec"
+ "context"
+ "os/exec"
 )
-
 type RuntimeCommand struct{}
-
-func (RuntimeCommand) Run(ctx context.Context, name string, args ...string) ([]byte, error) {
-	if err := ValidateRuntimeCommand(name); err != nil { return nil, err }
-	return exec.CommandContext(ctx, name, args...).CombinedOutput()
-}
+func (RuntimeCommand) Run(ctx context.Context,name string,args ...string)([]byte,error){if err:=ValidateRuntimeCommand(name);err!=nil{return nil,err};if err:=ValidateRuntimeArgs(args);err!=nil{return nil,err};return exec.CommandContext(ctx,name,args...).CombinedOutput()}
