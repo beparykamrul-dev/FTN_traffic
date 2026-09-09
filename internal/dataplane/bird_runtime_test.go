31c4521030e8c6a82dcfeb8867a0ee3d3a99c35b
Kamrul islam <227916250+beparykamrul-dev@users.noreply.github.com>
2026-09-09 15:55:51 -0600

    Test BIRD runtime command failures

--- internal/dataplane/bird_runtime_test.go
+++ internal/dataplane/bird_runtime_test.go
@@ -1,4 +1,7 @@
 package dataplane
 
 import("context";"errors";"testing")
+
 func TestBIRDRuntimePolicy(t *testing.T){r:=&fakeCommandRunner{};b:=BIRDRuntime{Runner:r,Authorized:true};if err:=b.Apply(context.Background(),"");!errors.Is(err,ErrApprovalRequired){t.Fatalf("got %v",err)};b.Approved=true;if err:=b.Apply(context.Background(),"");!errors.Is(err,ErrRuntimeCommandInvalid){t.Fatalf("got %v",err)}}
+
+func TestBIRDRuntimePropagatesRunnerError(t *testing.T){r:=&fakeCommandRunner{err:errors.New("birdc failed")};b:=BIRDRuntime{Runner:r,Authorized:true,Approved:true};if err:=b.Apply(context.Background(),"show protocols");err==nil||err.Error()!="birdc failed"{t.Fatalf("got %v",err)}}
