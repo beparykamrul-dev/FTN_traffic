a4d3864f14a0e142020f5080d2ee9d8930e3432f
Kamrul islam <227916250+beparykamrul-dev@users.noreply.github.com>
2026-09-09 20:41:29 +0600

    feat: initialize traffic store in service

--- cmd/ftn-traffic/main.go
+++ cmd/ftn-traffic/main.go
@@ -6,11 +6,13 @@ import (
 	"os"
 
 	"github.com/beparykamrul-dev/FTN_traffic/internal/httpapi"
+	"github.com/beparykamrul-dev/FTN_traffic/internal/traffic"
 )
 
 func main() {
 	addr := os.Getenv("FTN_TRAFFIC_ADDR")
 	if addr == "" { addr = ":8080" }
+	store := traffic.NewStore()
 	log.Printf("ftn-traffic listening on %s", addr)
-	if err := http.ListenAndServe(addr, httpapi.Handler()); err != nil { log.Fatal(err) }
+	if err := http.ListenAndServe(addr, httpapi.Handler(store)); err != nil { log.Fatal(err) }
 }
