074c436f91b4daf06c1c6f96e3309ec9f26ad3f9
Kamrul islam <227916250+beparykamrul-dev@users.noreply.github.com>
2026-09-09 13:17:57 -0600

    Validate runtime command arguments

--- internal/dataplane/command_runner.go
+++ internal/dataplane/command_runner.go
@@ -1,14 +1,15 @@
 package dataplane
 
 import (
- "context"
- "os/exec"
+	"context"
+	"os/exec"
 )
 
-type CommandRunner interface { Run(context.Context,string,...string)([]byte,error) }
+type CommandRunner interface { Run(context.Context, string, ...string) ([]byte, error) }
 type LocalCommandRunner struct{}
 
-func (LocalCommandRunner) Run(ctx context.Context,name string,args ...string)([]byte,error) {
- if err:=ValidateRuntimeCommand(name); err!=nil{return nil,err}
- return exec.CommandContext(ctx,name,args...).CombinedOutput()
+func (LocalCommandRunner) Run(ctx context.Context, name string, args ...string) ([]byte, error) {
+	if err := ValidateRuntimeCommand(name); err != nil { return nil, err }
+	if err := ValidateRuntimeArgs(args); err != nil { return nil, err }
+	return exec.CommandContext(ctx, name, args...).CombinedOutput()
 }
