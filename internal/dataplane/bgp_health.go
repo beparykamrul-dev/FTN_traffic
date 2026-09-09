b544fc5acc218fef6cc8c3f8cb63ea5877ede752
Kamrul islam <227916250+beparykamrul-dev@users.noreply.github.com>
2026-09-09 16:34:36 -0600

    dataplane: centralize BGP session health validation

--- internal/dataplane/bgp_health.go
+++ internal/dataplane/bgp_health.go
@@ -5,11 +5,17 @@ import "errors"
 var ErrNoEstablishedBGP = errors.New("no established BGP session")
 
 func ValidateBGPSessions(sessions []BGPSession, requireRPKI bool) error {
-	if len(sessions) == 0 { return ErrNoEstablishedBGP }
+	return ValidateBGPSessionsWithLimit(sessions, requireRPKI, 0)
+}
+
+func ValidateBGPSessionsWithLimit(sessions []BGPSession, requireRPKI bool, maxPrefixes uint64) error {
+	if len(sessions) == 0 {
+		return ErrNoEstablishedBGP
+	}
 	for _, s := range sessions {
-		if !s.Established { continue }
-		if requireRPKI && !s.RPKIValid { continue }
-		return nil
+		if s.Healthy(requireRPKI, maxPrefixes) {
+			return nil
+		}
 	}
 	return ErrNoEstablishedBGP
 }
