f2ef5aee64e96bc6965288af3a33760100d2b86b
Kamrul islam <227916250+beparykamrul-dev@users.noreply.github.com>
2026-09-09 16:48:28 -0600

    enforce BGP max-prefix health gate

--- internal/dataplane/health_gate.go
+++ internal/dataplane/health_gate.go
@@ -2,13 +2,26 @@ package dataplane
 
 import "context"
 
-type HealthGate struct { Router RouterAdapter; BGP BGPAdapter; RequireRPKI bool; MaxPrefixes uint64 }
+type HealthGate struct {
+	Router      RouterAdapter
+	BGP         BGPAdapter
+	RequireRPKI bool
+	MaxPrefixes uint64
+}
 
 func (g HealthGate) Check(ctx context.Context) error {
-	if g.Router == nil { return ErrBackendUnavailable }
-	if err := g.Router.Health(ctx); err != nil { return err }
-	if g.BGP == nil { return ErrBackendUnavailable }
+	if g.Router == nil {
+		return ErrBackendUnavailable
+	}
+	if err := g.Router.Health(ctx); err != nil {
+		return err
+	}
+	if g.BGP == nil {
+		return ErrBackendUnavailable
+	}
 	sessions, err := g.BGP.SessionSummary(ctx)
-	if err != nil { return err }
-	return ValidateBGPSessions(sessions, g.RequireRPKI)
+	if err != nil {
+		return err
+	}
+	return ValidateBGPSessionsWithLimit(sessions, g.RequireRPKI, g.MaxPrefixes)
 }
