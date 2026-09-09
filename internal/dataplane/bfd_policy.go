2e9794b8c7738a9caa1965ce4b2632068072f6b5
Kamrul islam <227916250+beparykamrul-dev@users.noreply.github.com>
2026-09-09 16:23:10 -0600

    canonicalize BFD endpoint comparison

--- internal/dataplane/bfd_policy.go
+++ internal/dataplane/bfd_policy.go
@@ -2,10 +2,4 @@ package dataplane
 
 import "net/netip"
 
-func ValidateBFD(s BFDSession) error {
-	if err := s.Validate(); err != nil { return err }
-	local, _ := netip.ParseAddr(s.Local)
-	remote, _ := netip.ParseAddr(s.Remote)
-	if local == remote { return ErrBFDInvalid }
-	return nil
-}
+func ValidateBFD(s BFDSession) error { if err:=s.Validate();err!=nil{return err}; local,_:=netip.ParseAddr(s.Local);remote,_:=netip.ParseAddr(s.Remote);if local==remote{return ErrBFDInvalid};return nil }
